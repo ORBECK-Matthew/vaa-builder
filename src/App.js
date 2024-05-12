4e50c0af0a5e32e47d57602308888aae47491078
ORBECK Matthew <orbeckmatthew@gmail.com>
2024-05-12 12:37:31 +1100

    Add ocean plane with texture

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+// Libraries
 import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Suspense } from "react";
+
+// Components
 import { Interface } from "./components/Interface";
 import { CameraControls } from "./components/CameraControls";
+import { Ocean } from "./components/Ocean";
 
 export default function App() {
   return (
     <div className="h-screen">
       <Canvas>
         <CameraControls />
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[3, 3, 5]} />
+        {/* <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[3, 3, 5]} /> */}
+        <directionalLight
+          intensity={5}
+          position={[-5, 5, 5]}
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
+        <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
         <mesh>
           <boxGeometry />
           <meshStandardMaterial />
         </mesh>
+        <Suspense fallback={null}>
+          <Ocean />
+        </Suspense>
       </Canvas>
       <Interface />
     </div>
